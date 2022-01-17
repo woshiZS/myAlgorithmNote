379a7c3c68885d9466cf751855149134548d9d25
Jason Heywood <jasonzhansen@foxmail.com>
2022-01-17 17:52:46 +0800

    modifications and creation of makefile

--- dp/spaceCutDp.c
+++ dp/spaceCutDp.c
@@ -4,29 +4,32 @@ int max(int left, int right){
   return left > right ? left : right;
 }
 
-void singleCondition(int n, int W, int* values, int* weights){
-  return values == NULL || weights == NULL;  
+int singleCondition(int n, int W, int* values, int* weights){
+  if(values == NULL || weights == NULL)
+    return -1;
   int dp[W + 1];
   memset(dp, 0, sizeof(dp));
   for(int i = 1; i <= n; ++i){
     int tempVal = values[i - 1];
     int tempWeigh = weights[i - 1];
-    for(int j = W; j >= tempVal; --j){
+    for(int j = W; j >= tempWeigh; --j){
       dp[j] = max(dp[j], dp[j - tempWeigh] + tempVal);
     }
   }
   return dp[W];
 }
 
-void multiCondition(int n, int W, int* valuse, int* weights){
-  return values == NULL || weights == NULL;
+int multiCondition(int n, int W, int* values, int* weights){
+  if(values == NULL || weights == NULL)
+    return -1;
   int dp[W + 1];
   memset(dp, 0, sizeof(dp));
   for(int i = 1; i <= n; ++i){
     int tempVal = values[i - 1];
     int tempWeigh = weights[i - 1];
-    for(int j = temp; j <= W; ++j){
+    for(int j = tempWeigh; j <= W; ++j){
       dp[j] = max(dp[j], dp[j - tempWeigh] + tempVal);
     }
   }
+  return dp[W];
 }
--- dp/test.c
+++ dp/test.c
@@ -5,7 +5,7 @@ int main(){
   int n = 3, W = 7;
   int weights[3] = {3, 4, 2};
   int values[3] = {4, 5, 3};
-  int singlerResult = singleCondition(n, W, values, weights);
+  int singleResult = singleCondition(n, W, values, weights);
   int multiResult = multiCondition(n, W, values, weights);
   printf("Single Result: %d\nMulti Result: %d\n", singleResult, multiResult);
   return 0;
